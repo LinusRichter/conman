f8e28ad9264eaef2991ac10ea304730de41c57b1
LinusRichter <linus.35287@gmail.com>
2024-05-17 00:22:32 +0200

    edit state

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,10 @@ use crossterm::{
     event::{self, KeyCode, KeyEventKind},
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen, }, ExecutableCommand,
 };
-use ratatui::{prelude::{CrosstermBackend, Stylize, Terminal}, widgets::Paragraph};
+use ratatui::{prelude::{CrosstermBackend, Terminal}};
 use std::io::{stdout, Result};
 use std::time::Duration;
 use crossterm::event::{Event, poll};
-use ratatui::layout::Rect;
-use ratatui::widgets::Widget;
 use crate::statemanager::StateManager;
 
 fn main() -> Result<()> {
@@ -19,24 +17,29 @@ fn main() -> Result<()> {
     terminal.clear()?;
 
     let mut state_manager = StateManager::new();
-    let mut state = state_manager.get_current_state();
     let mut index: u32 = 0;
 
     loop {
-        terminal.draw(|frame| {
-            state_manager.render(frame);
-        })?;
+        terminal.draw(|frame| { state_manager.render(frame); })?;
 
         if poll(Duration::from_secs(1))? {
             if let Event::Key(key) = event::read()? {
-                if(key.kind == KeyEventKind::Press) {
+                if key.kind == KeyEventKind::Press {
                     match key.code {
-                        KeyCode::Backspace => { state = state_manager.back(); }
-                        KeyCode::Enter => { state = state_manager.next(index); }
-                        KeyCode::Left => { state = state_manager.back(); }
-                        KeyCode::Right => { state = state_manager.next(index); }
-                        KeyCode::Up => { if index >= 1 { index -= 1; } }
-                        KeyCode::Down => { index += 1 }
+                        KeyCode::Backspace => { state_manager.back(); }
+                        KeyCode::Enter => { state_manager.next(index); }
+                        KeyCode::Left => { state_manager.back(); }
+                        KeyCode::Right => { state_manager.next(index); }
+                        KeyCode::Up => {
+                            if index >= 1 {
+                                index -= 1;
+                                state_manager.update_state_index(index);
+                            }
+                        }
+                        KeyCode::Down => {
+                            index += 1;
+                            state_manager.update_state_index(index);
+                        }
                         KeyCode::Esc => { break; }
                         _ => {}
                     }
